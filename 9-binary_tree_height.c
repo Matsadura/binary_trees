47858dee3ba522e255f6e12a1bb8fe1f13aac9cc
Matsadura <zidanditoo1@gmail.com>
2024-04-09 22:49:35 +0000

    [UPDATE] Add leaf condition

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_h, r_h;
 
-	if (tree == NULL)
+	if (tree == NULL || (!tree->left && !tree->right))
 		return (0);
 
 	l_h = binary_tree_height(tree->left);
