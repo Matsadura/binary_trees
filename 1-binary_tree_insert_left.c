79925501d4d474282156eab98244c9d7756c7eee
Matsadura <zidanditoo1@gmail.com>
2024-04-10 16:19:49 +0000

    [UPDATE] Remove comments

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,12 +15,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	/**
-	 * new_left = malloc(sizeof(binary_tree_t));
-	 * if (new_left == NULL)
-	 *	return (NULL);
-	 */
-
 	new_left = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
